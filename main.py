a5259769885a1ccdef21d54a8d82452ca494123d
Janhvi <janhvisingh1110@gmail.com>
2022-07-11 17:34:24 +0530

    integrate ARIMA model API

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Importing necessary modules
 import os
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from models import RequestBody
-from utils import load_model
+from utils import load_model, train_arima, generate_arima_prediction, check_csv
 
 # Declaring our FastAPI instance
 app = FastAPI()
@@ -34,12 +34,29 @@ async def predict(data : RequestBody, model: str = "MLP"):
     elif model == "LSTM":
         path = os.path.join(os.getcwd(), "LSTM")
     else:
-        return {'error': 'Invalid model type'}
+        raise HTTPException(status_code=400, detail="Invalid model type")
 
     model = load_model(path)
 
     for i in range(n_output_days):
         output = model.predict([data[i:]]).item(0)
         data.append(output)
+
+    return {'output': data}
+
+
+@app.post('/predict/upload')
+def predict_from_file(file: UploadFile, model: str = "ARIMA"):
+    if not check_csv(file.filename):
+        raise HTTPException(status_code=400, detail="Only CSV files allowed")
+    n_output_days = 7
+    
+    if model == "ARIMA":
+        arima_model = train_arima(file.file)
+        data = generate_arima_prediction(arima_model)
+    elif model == "PROPHET":
+        data = []
+    else:
+        raise HTTPException(status_code=400, detail="Invalid model type")
 
     return {'output': data}
